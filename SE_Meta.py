efd7b3ebc46fdaa6dcfad9df1bb2a4b988e3f725
Holmsoe <finn.holmsoe@gmail.com>
2020-02-26 22:28:41 +0100

    INstruction and comments

--- SE_Meta.py
+++ SE_Meta.py
@@ -1,4 +1,3 @@
-itorregistrete bank
 import requests
 import json
 
@@ -10,28 +9,32 @@ class SwedenMeta():
         SeHoved = 'https://api.scb.se/OV0104/v1/doris/en/ssd'
         r = requests.post(url = SeHoved)
         HovedTabel = r.text
-        return HovedTabel
+        d = json.loads(HovedTabel)
+        return d
        
     def TradeTables(self):
         # HA er "Trade in goods and services"
         SeUnder = 'https://api.scb.se/OV0104/v1/doris/en/ssd/HA'
         r = requests.post(url = SeUnder)
         UnderTabel = r.text
-        return UnderTabel
+        d = json.loads(UnderTabel)
+        return d
         
     def ForeignTradeTables(self):
         # HA0201 er "Foreign trade - exports and imports of goods"
         SeForeignTrade = 'https://api.scb.se/OV0104/v1/doris/en/ssd/HA/HA0201'
         r = requests.post(url = SeForeignTrade)
         ForeignTradeTabel = r.text 
-        return ForeignTradeTabel
+        d = json.loads(ForeignTradeTabel)
+        return d
         
     def ForeignTradeKNATables(self):
         # HA0201 er "Foreign trade - exports and imports of goods"
         SeKNA = 'https://api.scb.se/OV0104/v1/doris/en/ssd/HA/HA0201/HA0201B'
         r = requests.post(url = SeKNA)
         KNATabel = r.text
-        return KNATabel
+        d = json.loads(KNATabel)
+        return d
     
     def ExpTabelMetaDict(self):
         SEexpmeta = 'https://api.scb.se/OV0104/v1/doris/en/ssd/HA/HA0201/HA0201B/ExpTotalKNMan'
@@ -74,18 +77,134 @@ class SwedenMeta():
         varenavne=dict_varer['valueTexts']
         dict_varer=dict(zip(varenavne,varenumre))
         return dict_varer 
-      
-#minstat=SwedenMeta()       
-#print(minstat.MainTables())
-      
-#print(minstat.TradeTables())
-       
-#print(minstat.ForeignTradeTables())
-        
-#print(minstat.ForeignTradeKNATables())
 
-#exportmeta=minstat.ExpTabelMetaDict() 
-#print(exportmeta) 
+'''
+#Main tables      
+minstat=SwedenMeta()       
+mintabel=minstat.MainTables()
+for item in mintabel: print('\n',item['id'],item['text'])
+'''
+
+#============================================================
+
+'''
+#Trade tables      
+# use id=HA fra MainTables
+minstat=SwedenMeta()       
+mintabel=minstat.TradeTables()
+for item in mintabel: print('\n',item['id'],item['text'])
+'''
+
+#============================================================
+
+'''
+#Foreign Trade tables      
+# use id=HA0201 fra TradeTable
+minstat=SwedenMeta()       
+mintabel=minstat.ForeignTradeTables()
+for item in mintabel: print('\n',item['id'],item['text'])
+'''
+
+#============================================================
+
+'''
+#Foreign Trade tables combined nomenclature      
+# use id=HA0201B fra ForeignTradeTable
+minstat=SwedenMeta()       
+mintabel=minstat.ForeignTradeKNATables()
+for item in mintabel: print('\n',item['id'],item['text'])
+'''
+
+#============================================================
+
+'''
+#Export 8 digit commodity code 
+#variabel oversigt     
+# use id= ExpTotalKNMan fra TradeTable combined nomenclature
+minstat=SwedenMeta()       
+mintabel=minstat.ExpTabelMetaDict()
+for item in mintabel['variables']: print('\n',item['code'])
+'''
+
+#============================================================
+
+'''
+#Variabel export indhold
+minstat=SwedenMeta()       
+mintabel=minstat.ExpTabelMetaDict()
+# VarugruppKN (varegruppe) Handelspartner (land) ContentsCode (enhed) Tid (årmåned)
+
+#VarugruppKN
+#print(mintabel['variables'][0])
+#'values' = varekode
+#'valueTexts' = varenavn
+
+#Handelspartner
+#print(mintabel['variables'][1])
+#'values' = landekode
+#'valueTexts' = land
+
+#Enhed
+#print(mintabel['variables'][2])
+#values     valueTexts
+#HA0201H1 = 1000 kg
+#HA0201H2 = 1000 sek
+#HA0201H3 = supplementary unit 
+
+#Månedår
+#print(mintabel['variables'][3])
+#'values' = månedkode
+#'valueTexts' = måned (samme som månedkode)
+'''
+
+#============================================================
+
+'''
+#Import 8 digit commodity code 
+#variabel oversigt     
+# use id= ImpTotalKNMan fra TradeTable combined nomenclature
+minstat=SwedenMeta()       
+mintabel=minstat.ImpTabelMetaDict()
+for item in mintabel['variables']: print('\n',item['code'])
+'''
+
+#============================================================
+
+'''
+#Variabel import indhold
+minstat=SwedenMeta()       
+mintabel=minstat.ImpTabelMetaDict()
+# VarugruppKN (varegruppe) Handelspartner (land) ContentsCode (enhed) Tid (årmåned)
+
+#VarugruppKN
+#print(mintabel['variables'][0])
+#'values' = varekode
+#'valueTexts' = varenavn
+
+#Handelspartner=land
+#print(mintabel['variables'][1])
+#'values' = landekode
+#'valueTexts' = land
+
+#Enhed
+#print(mintabel['variables'][2])
+#values     valueTexts
+#HA0201H1 = 1000 kg
+#HA0201H2 = 1000 sek
+#HA0201H3 = supplementary unit 
+
+#Månedår
+#print(mintabel['variables'][3])
+#'values' = månedkode
+#'valueTexts' = måned (samme som månedkode)
+'''
+
+#============================================================
+
+'''
+#Export Country/Product dictionaries 
+minstat=SwedenMeta()
+exportmeta=minstat.ExpTabelMetaDict() 
 
 #print(minstat.CountryCodeNameDict(exportmeta))        
 #print(minstat.CountryNameCodeDict(exportmeta))
@@ -98,13 +217,17 @@ class SwedenMeta():
 #print(minstat.CountryNameCodeDict(importmeta))
 #print(minstat.ProductCodeNameDict(importmeta))        
 #print(minstat.ProductNameCodeDict(importmeta))
+'''
 
+
+'''
 #Eksempel - Find landekode fra exp register der svarer til landet "Norway"
-#exportmeta=minstat.ExpTabelMetaDict() #metatabel
-#landetabel=minstat.CountryNameCodeDict(exportmeta)  #lande dict med landenavn som indgangskode
-#kode=landetabel["Norway"]
-#print("Svaret er:",kode)
-
+minstat=SwedenMeta()
+exportmeta=minstat.ExpTabelMetaDict() #metatabel
+landetabel=minstat.CountryNameCodeDict(exportmeta)  #lande dict med landenavn som indgangskode
+kode=landetabel["Norway"]
+print("Svaret er:",kode)
+'''
 
 
 
